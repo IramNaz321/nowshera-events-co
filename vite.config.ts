8e4680785433f5bed116d4f1f14f6f894d47ed18
Iram Naz <irumnaaz2020@gmail.com>
2026-09-15 05:50:57 -0700

    chore: set base path for deployment
    Configure the base URL to support deployment on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/nowshera-events-co/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
